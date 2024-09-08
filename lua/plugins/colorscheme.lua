18889f5e03898b692cf1497490b26e8950422b67
roy <roydanner22@gmail.com>
2024-09-07 23:27:04 -0400

    new_color_schemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
     --'vague2k/vague.nvim',
-    'water-sucks/darkrose.nvim',
+    --'water-sucks/darkrose.nvim',
+    --'mhartington/oceanic-next',
+    'sho-87/kanagawa-paper.nvim',
     lazy = false,
     priority = 1000,
     config = function()
         vim.opt.termguicolors = true
         vim.g.sonokai_enable_italic = true
-        vim.cmd.colorscheme ('darkrose')
+        vim.cmd.colorscheme ('kanagawa-paper')
     end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts= {
-        options = { theme = 'iceberg_dark',
+        options = { theme = 'auto',
             section_separators = '', component_separators = ''
         }
     }
